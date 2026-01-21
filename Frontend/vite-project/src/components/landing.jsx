0f1c33d4e241cc939ebe7d0374b4193f065f7cca
PriyanshuRawat0000 <priyanshurawat3.1415@gmail.com>
2026-01-21 23:00:00 +0530

    added css for landing page

--- Frontend/vite-project/src/components/landing.jsx
+++ Frontend/vite-project/src/components/landing.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import {useNavigate,Navigate} from 'react-router-dom';
+import './landing.css';
 
 const Landing=()=>{
     const navigate = useNavigate();
     return (
-        <div>
-            <h1>Welcome to the Image Gallery</h1>
-            <h2>Explore and download your favorite images</h2>
+        
+        
+        <div className="landing">
             <div>
-                <button onClick={()=>navigate('/login')}>Login</button>
-                <button onClick={()=>navigate('/signUp')}>Sign Up</button>
+                <h1>Welcome to the Image Gallery</h1>
+                <h2>Explore and download your favorite images</h2>
+                <div>
+                    <button onClick={() => navigate('/login')}>Login</button>
+                    <button onClick={() => navigate('/signUp')}>Sign Up</button>
+                </div>
             </div>
         </div>
     )

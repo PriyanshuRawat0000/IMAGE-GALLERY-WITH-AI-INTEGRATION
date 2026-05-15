eca6afa09755e596aecf576a63efbdb43f512668
PriyanshuRawat0000 <priyanshurawat3.1415@gmail.com>
2026-05-15 13:59:24 +0530

    updating server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://ai-gallery-xi.vercel.app',
   credentials: true,
   optionsSuccessStatus: 200
 };

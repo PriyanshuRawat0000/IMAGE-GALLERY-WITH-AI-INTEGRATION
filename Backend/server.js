083aeecbaff3c6f52c7f1287f6d527ca9ce58f96
PriyanshuRawat0000 <priyanshurawat3.1415@gmail.com>
2026-06-18 16:59:18 +0530

    removed unused url from whitelisting

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
+    
     'https://ai-gallery-xi.vercel.app',
     'https://ai-gallery-mbx6muwuv-priyanshurawat31415-6531s-projects.vercel.app',
     'https://ai-gallery-git-main-priyanshurawat31415-6531s-projects.vercel.app'

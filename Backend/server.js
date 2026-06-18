be1983e64a6c2b6c4df94ce385ee91d1a84b5bd8
PriyanshuRawat0000 <priyanshurawat3.1415@gmail.com>
2026-06-19 01:41:08 +0530

    added responsiveness for phones and tablets

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 const corsOptions = {
   origin: [
-    
+   
     'https://ai-gallery-xi.vercel.app',
     'https://ai-gallery-mbx6muwuv-priyanshurawat31415-6531s-projects.vercel.app',
     'https://ai-gallery-git-main-priyanshurawat31415-6531s-projects.vercel.app'

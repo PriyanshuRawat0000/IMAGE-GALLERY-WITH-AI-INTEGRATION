6e2abae777622ecf9128ceedeafed44059e086dd
PriyanshuRawat0000 <priyanshurawat3.1415@gmail.com>
2026-06-18 17:09:50 +0530

    increased access token expiry limit

--- Backend/src/config/token.js
+++ Backend/src/config/token.js
@@ -5,7 +5,7 @@ const generateAccessToken = (user) => {
     return jwt.sign({
       userId: user._id,
       email: user.email
-    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10s' });
+    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
 };
 
 const generateRefreshToken = (user) => {
